81635a977b54b9a0cfe720bf375cfa373080dcc2
Priapisman677 <expeliamm02@hotmail.com>
2025-06-19 09:38:44 -0700

    Update

--- src/middleware/req-logger-middleware.ts
+++ src/middleware/req-logger-middleware.ts
@@ -4,14 +4,9 @@ import { writeFileSync } from 'fs';
 
 export const reqLogger = async(req: Request, res: Response, next: NextFunction)=>{
 
-    // console.log( req.headers['x-client-ip']);
-    // console.log(req.headers['x-forwarded-for']?.split(',')[0]?.trim());
-    // console.log(req.headers['cf-connecting-ip']);
-    // console.log(req.headers['true-client-ip']);
-    // console.log(req.headers['x-real-ip']);
-    // console.log(req.headers['x-cluster-client-ip']);
-    // console.log(req.connection?.remoteAddress);
-   console.log(req.socket?.remoteAddress);
+
+    console.log(req.socket?.remoteAddress);
+    console.log(req.ip);
     
     
     
